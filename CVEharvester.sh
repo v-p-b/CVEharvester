7e6872284b90b9ec741b7cada18e8d7fd5829d41
v-p-b <vpbalint@gmail.com>
2011-10-13 13:29:27 +0200

    silencing wget

--- CVEharvester.sh
+++ CVEharvester.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 mitre="https://cve.mitre.org/cgi-bin/cvename.cgi?name="
-wget -O cve.html $1
+wget -q -O cve.html $1
 
 for cve in `egrep 'CVE-[0-9]{4}-[0-9]{4}' cve.html | sed -r 's/.*(CVE-[0-9]{4}-[0-9]{4}).*/\1/g'`
 do
-    wget -O $cve.html "$mitre$cve"
+    wget -q -O $cve.html "$mitre$cve"
     if grep -q "RESERVED" $cve.html
     then
         rm $cve.html
