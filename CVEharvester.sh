b1fac1e3aaf26b9587fdb24dd3aa3d304ee6574e
v-p-b <vpbalint@gmail.com>
2011-10-13 12:42:01 +0200

    first commit

---
+++ CVEharvester.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mitre="https://cve.mitre.org/cgi-bin/cvename.cgi?name="
+wget -O cve.html $1
+
+for cve in `egrep 'CVE-[0-9]{4}-[0-9]{4}' cve.html | sed -r 's/.*(CVE-[0-9]{4}-[0-9]{4}).*/\1/g'`
+do
+    wget -O $cve.html "$mitre$cve"
+    if grep -q "RESERVED" $cve.html
+    then
+        rm $cve.html
+    else
+        echo $cve
+    fi        
+done
+
+rm cve.html
