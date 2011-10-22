ba264bb7d68eda7e156d5f5db7b47ad4fd026b74
v-p-b <vpbalint@gmail.com>
2011-10-22 19:37:13 +0200

    Handling multiple entries in one line of html

--- CVEharvester.sh
+++ CVEharvester.sh
@@ -3,11 +3,12 @@
 mitre="https://cve.mitre.org/cgi-bin/cvename.cgi?name="
 wget -q -O cve.html $1
 
-for cve in `egrep 'CVE-[0-9]{4}-[0-9]{4}' cve.html | sed -r 's/.*(CVE-[0-9]{4}-[0-9]{4}).*/\1/g'`
+for cve in `sed 's/>/>\n/g' cve.html | egrep 'CVE-[0-9]{4}-[0-9]{4}'  | sed -r 's/.*(CVE-[0-9]{4}-[0-9]{4}).*/\1/g'`
 do
     wget -q -O $cve.html "$mitre$cve"
     if grep -q "RESERVED" $cve.html
     then
+        echo "Skipped" $cve
         rm $cve.html
     else
         echo $cve
