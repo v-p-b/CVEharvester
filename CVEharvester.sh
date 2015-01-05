cd42958ea61e154ef43639385e523e23e6511ec0
B <buherator@silentsignal.hu>
2015-01-05 10:14:15 +0100

    Added support for 4+ digits CVEs

--- CVEharvester.sh
+++ CVEharvester.sh
@@ -3,7 +3,7 @@
 mitre="https://cve.mitre.org/cgi-bin/cvename.cgi?name="
 wget -q -O cve.html $1
 
-for cve in `sed 's/>/>\n/g' cve.html | egrep 'CVE-[0-9]{4}-[0-9]{4}'  | sed -r 's/.*(CVE-[0-9]{4}-[0-9]{4}).*/\1/g'`
+for cve in `sed 's/>/>\n/g' cve.html | egrep 'CVE-[0-9]{4}-[0-9]{4,}'  | sed -r 's/.*(CVE-[0-9]{4}-[0-9]{4,}).*/\1/g'`
 do
     wget -q -O $cve.html "$mitre$cve"
     if grep -q "RESERVED" $cve.html
